9492f2ac5325d4252e41c7c2e09a9dbf9b8037f8
Park Billy <rtlink.park@gmail.com>
2018-11-05 18:00:44 +0900

    안드로이드 앱 소개 페이지 추가

--- pages/apps.vue
+++ pages/apps.vue
@@ -1,47 +1,21 @@
 <template>
 
   <v-container>
-    <h1 class="mb-3">for iOS</h1>
+    <h1>Download Mobile App</h1>
 
-    <div class="img-cont">
-      <div v-for="(item, i) in items" :key="i" class="img-item">
-        <img :src="item.src" width="200px"/>
+    <div style="text-align: center" class="mt-4">
+      <div class="mb-5">
+        <img src="@/assets/images/iphonex.png" height="400px"/>
+        <img src="@/assets/images/android.png" height="400px"/>
       </div>
-    </div>
 
-    <div style="text-align: center" class="mt-5">
       <a href="https://itunes.apple.com/kr/app/id1440408457?mt=8" target="_blank">
-        <img src="@/assets/images/apple_download.svg" width="160px">
+        <img src="@/assets/images/apple-badge-ko.png" width="180px">
+      </a>
+      <a href="https://play.google.com/store/apps/details?id=kr.practical.my_tube" target="_blank">
+        <img src="@/assets/images/google-badge-ko.png" width="180px">
       </a>
     </div>
   </v-container>
 
 </template>
-
-<script>
-  export default {
-  	data() {
-  		return {
-  			items: [
-          { src :"https://s3.ap-northeast-2.amazonaws.com/rtlink/app/app_ios1.jpg" },
-				  { src :"https://s3.ap-northeast-2.amazonaws.com/rtlink/app/app_ios2.jpg" },
-				  { src :"https://s3.ap-northeast-2.amazonaws.com/rtlink/app/app_ios3.jpg" },
-				  { src :"https://s3.ap-northeast-2.amazonaws.com/rtlink/app/app_ios4.jpg" },
-				  { src :"https://s3.ap-northeast-2.amazonaws.com/rtlink/app/app_ios5.jpg" }
-        ]
-      }
-    }
-  }
-</script>
-
-<style scoped>
-  .img-cont {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
-  }
-
-  .img-item {
-    display: inline-block;
-  }
-</style>
